2a2fad664e49d104b7b09bc3148711b51503c98c
BLOOD212 <g95829914@gmail.com>
2026-05-16 14:56:02 +0200

    Update anti-link.js

--- plugins/anti-link.js
+++ plugins/anti-link.js
@@ -65,7 +65,7 @@ async function handleViolation(conn, m, reason, isBotAdmin) {
 
     const text = `${header}
 ╭
-┃ 🚨 \`Stato:\` *Protocollo Blood Attivo*
+┃ 🚨 \`Stato:\` *Protocollo Attivo*
 ┃
 ┃ 『 👤 』 \`Target:\` @${sender.split('@')[0]}
 ┃ 『 🚫 』 \`Azione:\` *Messaggio Rimosso*
@@ -79,7 +79,7 @@ async function handleViolation(conn, m, reason, isBotAdmin) {
         mentions: [sender],
         contextInfo: {
             externalAdReply: {
-                title: 'BLOOD SECURITY SYSTEM',
+                title: '𝐏𝐚𝐧𝐝𝐢-𝐁𝐨𝐭',
                 body: 'Link vietato rilevato',
                 thumbnailUrl: 'https://qu.ax/TfUj.jpg',
                 mediaType: 1,
